6884ff2f36330314decb0b7b7ad4d84d395c25c6
abhaygore <abhaydhanrajgore@gmail.com>
2023-09-07 15:32:32 +0530

    Initial commit

---
+++ src/main/java/com/example/CustomerService_v2/CustomerServiceV2Application.java
@@ -0,0 +1,13 @@
+package com.example.CustomerService_v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerServiceV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerServiceV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CustomerService_v2/CustomerServiceV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CustomerService_v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerServiceV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
