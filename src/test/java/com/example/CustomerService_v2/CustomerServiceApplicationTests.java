7e83c7086b28141aa830689f6e31d40e8c92b0c1
abhaygore <abhaydhanrajgore@gmail.com>
2023-09-08 17:46:56 +0530

    issue 1- File name changed

--- src/test/java/com/example/CustomerService_v2/CustomerServiceApplicationTests.java
+++ src/test/java/com/example/CustomerService_v2/CustomerServiceApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CustomerServiceV2ApplicationTests {
+class CustomerServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
