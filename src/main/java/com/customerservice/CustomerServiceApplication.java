6866b0e1cfc8438ad345318165d9e6887146f792
abhaygore <abhaydhanrajgore@gmail.com>
2023-09-14 14:39:25 +0530

    issue 4- Requested changes have been made changed package name and removed empty spaces

--- src/main/java/com/customerservice/CustomerServiceApplication.java
+++ src/main/java/com/customerservice/CustomerServiceApplication.java
@@ -1,4 +1,4 @@
-package com.CustomerService;
+package com.customerservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
